9910c19e93673342579dd5f6ce6e4e4bcc165df0
Eugene Linchenko <e.linchenko@favbet.com>
2015-05-26 14:12:15 +0300

    remove old flatten impl

--- p07.erl
+++ p07.erl
@@ -1,26 +1,16 @@
 -module(p07).
--export([flatten/1, flatten2/1]).
+-export([flatten/1]).
 -import(p05, [reverse/1]).
+
 
 flatten(L) ->
     flatten(L, []).
 
-flatten([[_|_] = H|T], A) ->
-    flatten(H ++ T, A);
+flatten([[HH]|T], A) ->
+    flatten([HH|T], A);
+flatten([[HH|HT]|T], A) ->
+    flatten([HH,HT|T], A);
 flatten([H|T], A) ->
     flatten(T, [H|A]);
 flatten([], A) ->
-    reverse(A).
-
-
-flatten2(L) ->
-    flatten2(L, []).
-
-flatten2([[HH]|T], A) ->
-    flatten2([HH|T], A);
-flatten2([[HH|HT]|T], A) ->
-    flatten2([HH,HT|T], A);
-flatten2([H|T], A) ->
-    flatten2(T, [H|A]);
-flatten2([], A) ->
     reverse(A).
