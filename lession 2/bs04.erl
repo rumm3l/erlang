1bdd78e70a1cacfdf63bb6d80bd19b03c097a6f3
Eugene Linchenko <e.linchenko@favbet.com>
2015-05-29 01:33:40 +0300

    WIP

--- lession 2/bs04.erl
+++ lession 2/bs04.erl
@@ -1,31 +1,37 @@
 -module(bs04).
--export([decode_xml/1, get_tag_name/1, push_element/2, pop_element/1]).
+-export([decode_xml/1]).
 
 decode_xml(BinStr) ->
     decode_xml(BinStr, [], <<>>).
 
 
-decode_xml(<<>>, [RootElement], TextBuf) ->
-    RootElement;
-decode_xml(<<"</", DocStream/binary>>, Stack, TextBuf) ->
-    [CurrElement|[PrevElement|NewStack]] = Stack,
+decode_xml(<<"</", DocStream/binary>>, [CurrElement, PrevElement|Stack], TextBuf) ->
     {_, NewDocStream} = get_tag_name(DocStream),
-    decode_xml(NewDocStream, [push_element(PrevElement, CurrElement)|NewStack], <<>>);
-decode_xml(<<"<", DocStream/binary>>, Stack, TextBuf) ->
+    decode_xml(NewDocStream, [push_to_element(PrevElement, push_to_element(CurrElement,TextBuf))|Stack], <<>>);
+decode_xml(<<"</", _/binary>>, [RootElement], _) ->
+    RootElement;
+decode_xml(<<"<", DocStream/binary>>, [CurrElement|Stack], TextBuf) ->
+    {TagName, NewDocStream} = get_tag_name(DocStream),
+    decode_xml(NewDocStream, [{TagName, [], []}, push_to_element(CurrElement, TextBuf)|Stack], <<>>);
+decode_xml(<<"<", DocStream/binary>>, Stack, _) ->
     {TagName, NewDocStream} = get_tag_name(DocStream),
     decode_xml(NewDocStream, [{TagName, [], []}|Stack], <<>>);
 decode_xml(<<Char, DocStream/binary>>, Stack, TextBuf) ->
     decode_xml(DocStream, Stack, <<TextBuf/binary, Char>>).
 
 
+push_to_head(Stack, <<>>) ->
+    Stack;
+push_to_head([Head|Stack], Value)->
+    [push_to_element(Head, Value)|Stack];
+push_to_head(Stack, _) ->
+    Stack.
 
 
-push_element(Root, Element) ->
-    {ElementName, _, Elements} = Root,
-    {ElementName, [], [Element|Elements]}.
+push_to_element(Element, Value) ->
+    {ElementName, _, Values} = Element,
+    {ElementName, [], [Value|Values]}.
 
-pop_element([Element|Stack]) ->
-    {Element, Stack}.
 
 get_tag_name(DocStream) ->
     get_tag_name(DocStream, <<>>).
