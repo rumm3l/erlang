4a18b3622b594cf97962301a0d392b3e22458707
Eugene Linchenko <e.linchenko@favbet.com>
2015-06-14 23:34:53 +0300

    add TTLed lookup_by_date

--- lession 4/cache_api.erl
+++ lession 4/cache_api.erl
@@ -8,6 +8,7 @@
 -export([lookup/2]).
 -export([lookup/3]).
 -export([lookup_by_date/3]).
+-export([lookup_by_date/4]).
 -export([get_ts/0]).
 -export([dt_2_ts/1]).
 
@@ -41,42 +42,42 @@ clean(Table) ->
   ets:delete_all_objects(Table).
 
 lookup(Table, Key) ->
-  case ets:lookup(Table, Key) of
-    [Value] ->
-      Value;
-    _ ->
-      undefined
-  end.
+  lookup(Table, Key, get_ts()). % simply lookup with extra big ttl
 
 lookup(Table, Key, TTL) ->
   CTS = get_ts(),
   case ets:lookup(Table, Key) of
     [#entry{timestamp = ETS, key = Key}] when CTS > ETS + TTL ->
+      io:format("Remove stale~n"),
       delete(Table, Key), % remove stale
       undefined;
-    [#entry{value = Value, key = Key}] ->
+    [#entry{value = Value, key = Key} = Entry] ->
       insert(Table, Key, Value, CTS), % prolong TTL
-      Value;
+      Entry;
     _ ->
       undefined
   end.
 
 
 lookup_by_date(Table, FromTimestamp, ToTimestamp) ->
-  Key = ets:first(Table),
-  lookup_by_date(Table, FromTimestamp, ToTimestamp, Key, []).
+  lookup_by_date(Table, FromTimestamp, ToTimestamp, get_ts()).
 
-lookup_by_date(_Table, _FromTs, _ToTs, '$end_of_table', Entries) ->
+lookup_by_date(Table, FromTimestamp, ToTimestamp, TTL) ->
+  Key = ets:first(Table),
+  lookup_by_date(Table, FromTimestamp, ToTimestamp, Key, [], TTL).
+
+lookup_by_date(_Table, _FromTs, _ToTs, '$end_of_table', Entries, _TTL) ->
   Entries;
-lookup_by_date(Table, FromTs, ToTs, Key, Entries) ->
-  #entry{timestamp = Ts} = Entry = lookup(Table, Key),
-  NewEntries = if Ts >= FromTs andalso Ts =< ToTs ->
+lookup_by_date(Table, FromTs, ToTs, Key, Entries, TTL) ->
+  NewEntries = case lookup(Table, Key, TTL) of
+                 #entry{timestamp = Ts} = Entry when Ts >= FromTs andalso Ts =< ToTs ->
                    [Entry|Entries];
-                  true ->
+                 _ ->
                    Entries
-               end,
+  end,
+
   NextKey = ets:next(Table, Key),
-  lookup_by_date(Table, FromTs, ToTs, NextKey, NewEntries).
+  lookup_by_date(Table, FromTs, ToTs, NextKey, NewEntries, TTL).
 
 test() ->
   Table = init(),
@@ -86,9 +87,11 @@ test() ->
   insert(Table, one, "ONE", TS1),
   insert(Table, two, "TWO", TS2),
   insert(Table, three, "THREE", TS3),
+  io:format("Data filled~n"),
   io:format("lookup1 ~p~n", [test_lookup1(Table)]),
   io:format("lookup2 ~p~n", [test_lookup2(Table)]),
   io:format("lookup3 ~p~n", [test_lookup3(Table)]),
+  io:format("lookup4 ~p~n", [test_lookup4(Table)]),
   ok.
 
 test_lookup1(Table) ->
@@ -102,4 +105,7 @@ test_lookup2(Table) ->
   lookup_by_date(Table, TS1, TS2).
 
 test_lookup3(Table) ->
-  lookup_by_date(Table, 100, 200).
+  lookup_by_date(Table, get_ts() - 3600, get_ts()).
+
+test_lookup4(Table) ->
+  lookup_by_date(Table, 0, get_ts(), -1).
