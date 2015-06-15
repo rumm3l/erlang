d8a780e0571bc1c34edff750ed520b84dd7e1d11
Eugene Linchenko <e.linchenko@favbet.com>
2015-06-15 22:17:59 +0300

    add cache-server impl with timer

--- lession 4/cache_api.erl
+++ lession 4/cache_api.erl
@@ -6,9 +6,9 @@
 -export([delete/2]).
 -export([clean/1]).
 -export([lookup/2]).
--export([lookup/3]).
+-export([lookup/4]).
 -export([lookup_by_date/3]).
--export([lookup_by_date/4]).
+-export([lookup_by_date/5]).
 -export([get_ts/0]).
 -export([dt_2_ts/1]).
 
@@ -42,16 +42,20 @@ clean(Table) ->
   ets:delete_all_objects(Table).
 
 lookup(Table, Key) ->
-  lookup(Table, Key, get_ts()). % simply lookup with extra big ttl
+  lookup(Table, Key, get_ts(), true). % simply lookup with extra big ttl
 
-lookup(Table, Key, TTL) ->
+lookup(Table, Key, TTL, Prolong) ->
   CTS = get_ts(),
   case ets:lookup(Table, Key) of
     [#entry{timestamp = ETS, key = Key}] when CTS > ETS + TTL ->
       delete(Table, Key), % remove stale
+      io:format("found old~n"),
       undefined;
     [#entry{value = Value, key = Key} = Entry] ->
-      insert(Table, Key, Value, CTS), % prolong TTL
+      if Prolong == true -> insert(Table, Key, Value, CTS);
+         true -> void
+      end,
+      io:format("found valid~n"),
       Entry;
     _ ->
       undefined
@@ -59,16 +63,17 @@ lookup(Table, Key, TTL) ->
 
 
 lookup_by_date(Table, FromTimestamp, ToTimestamp) ->
-  lookup_by_date(Table, FromTimestamp, ToTimestamp, get_ts()).
+  lookup_by_date(Table, FromTimestamp, ToTimestamp, get_ts(), true).
 
-lookup_by_date(Table, FromTimestamp, ToTimestamp, TTL) ->
+lookup_by_date(Table, FromTimestamp, ToTimestamp, TTL, Prolong) ->
+  io:format("LOOKUP ~p ~p ~p ~p~n", [FromTimestamp, ToTimestamp, TTL, Prolong]),
   Key = ets:first(Table),
-  lookup_by_date(Table, FromTimestamp, ToTimestamp, Key, [], TTL).
+  lookup_by_date(Table, FromTimestamp, ToTimestamp, Key, [], TTL, Prolong).
 
-lookup_by_date(_Table, _FromTs, _ToTs, '$end_of_table', Entries, _TTL) ->
+lookup_by_date(_Table, _FromTs, _ToTs, '$end_of_table', Entries, _TTL, _Prolong) ->
   Entries;
-lookup_by_date(Table, FromTs, ToTs, Key, Entries, TTL) ->
-  NewEntries = case lookup(Table, Key, TTL) of
+lookup_by_date(Table, FromTs, ToTs, Key, Entries, TTL, Prolong) ->
+  NewEntries = case lookup(Table, Key, TTL, Prolong) of
                  #entry{timestamp = Ts} = Entry when Ts >= FromTs andalso Ts =< ToTs ->
                    [Entry|Entries];
                  _ ->
@@ -76,35 +81,4 @@ lookup_by_date(Table, FromTs, ToTs, Key, Entries, TTL) ->
   end,
 
   NextKey = ets:next(Table, Key),
-  lookup_by_date(Table, FromTs, ToTs, NextKey, NewEntries, TTL).
-
-test() ->
-  Table = init(),
-  TS1 = datetime_to_gregorian_seconds({{2015, 6, 1}, {0, 0, 0}}),
-  TS2 = datetime_to_gregorian_seconds({{2015, 6, 2}, {0, 0, 0}}),
-  TS3 = datetime_to_gregorian_seconds({{2015, 6, 3}, {0, 0, 0}}),
-  insert(Table, one, "ONE", TS1),
-  insert(Table, two, "TWO", TS2),
-  insert(Table, three, "THREE", TS3),
-  io:format("Data filled~n"),
-  io:format("lookup1 ~p~n", [test_lookup1(Table)]),
-  io:format("lookup2 ~p~n", [test_lookup2(Table)]),
-  io:format("lookup3 ~p~n", [test_lookup3(Table)]),
-  io:format("lookup4 ~p~n", [test_lookup4(Table)]),
-  ok.
-
-test_lookup1(Table) ->
-  TS1 = datetime_to_gregorian_seconds({{2015, 6, 1}, {0, 0, 0}}),
-  TS2 = datetime_to_gregorian_seconds({{2015, 6, 2}, {0, 0, 0}}),
-  lookup_by_date(Table, TS1, TS2).
-
-test_lookup2(Table) ->
-  TS1 = datetime_to_gregorian_seconds({{2015, 6, 2}, {0, 0, 0}}),
-  TS2 = datetime_to_gregorian_seconds({{2015, 6, 4}, {0, 0, 0}}),
-  lookup_by_date(Table, TS1, TS2).
-
-test_lookup3(Table) ->
-  lookup_by_date(Table, get_ts() - 3600, get_ts()).
-
-test_lookup4(Table) ->
-  lookup_by_date(Table, 0, get_ts(), -1).
+  lookup_by_date(Table, FromTs, ToTs, NextKey, NewEntries, TTL, Prolong).
